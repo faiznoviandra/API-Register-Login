3cc9b4b9e05128cc4cd539c6d522b897e5a598af
faiz haq noviandra ciptadi putra <116566988+faiznoviandra@users.noreply.github.com>
2024-07-04 02:47:22 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,15 @@
-const mysql = require('mysql2');
+// Mengimpor library mysql2 untuk koneksi ke database MySQL
+const mysql = require('mysql2');  
+
+// Mengimpor dan mengkonfigurasi dotenv untuk mengelola variabel lingkungan dari file .env
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,         // Mengambil host database dari variabel lingkungan
+  user: process.env.DB_USER,         // Mengambil username database dari variabel lingkungan
+  password: process.env.DB_PASSWORD, // Mengambil password database dari variabel lingkungan
+  database: process.env.DB_NAME,     // Mengambil nama database dari variabel lingkungan
 });
 
-module.exports = pool.promise();
+module.exports = pool.promise();    // Mengekspor pool koneksi sebagai promise
+
